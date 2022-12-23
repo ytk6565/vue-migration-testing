e55506be72d53e2039ead0618823ca032650bbdf
ytk6565 <ytk6565@gmail.com>
2022-12-23 09:41:19 +0900

    cypress に mount コマンドを追加すると jest の型と衝突する個別に import
    https://github.com/cypress-io/cypress/issues/22059

--- packages/ui/src/VCounter/__tests__/component.cy.ts
+++ packages/ui/src/VCounter/__tests__/component.cy.ts
@@ -1,23 +1,21 @@
+import { mount } from 'cypress/vue'
+
 import VCounter from "../index.vue";
 
-describe("<VCounter />", () => {
+describe("<VCounter />", ()  => {
   it("renders", () => {
-    // TODO: 型エラーを修正する
-    // @ts-ignore
-    cy.mount(VCounter);
+    mount(VCounter);
     cy.get("[data-cy=counter]").should("have.text", "Times clicked: 0");
   });
 
   it("when the increment button is pressed, the counter is incremented", () => {
-    // @ts-ignore
-    cy.mount(VCounter);
+    mount(VCounter);
     cy.get("[data-cy=increment]").click();
     cy.get("[data-cy=counter]").should("have.text", "Times clicked: 1");
   });
 
   it("when the decrement button is pressed, the counter is decremented", () => {
-    // @ts-ignore
-    cy.mount(VCounter);
+    mount(VCounter);
     cy.get("[data-cy=decrement]").click();
     cy.get("[data-cy=counter]").should("have.text", "Times clicked: -1");
   });
