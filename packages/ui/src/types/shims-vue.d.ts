71e21078bcdf14dca0dcba204f175a3283791b1f
ytk6565 <ytk6565@gmail.com>
2022-12-24 22:24:30 +0900

    shims-vue.d.ts を修正

--- packages/ui/src/types/shims-vue.d.ts
+++ packages/ui/src/types/shims-vue.d.ts
@@ -1,5 +1,10 @@
 declare module "*.vue" {
-  import type { DefineComponent } from "vue-demi";
+  import Vue from "vue";
+  export default Vue;
+}
+
+declare module "*.vue" {
+  import type { DefineComponent } from "vue3";
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
