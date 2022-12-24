2e2ae818cbe0c89384f242ced94f636e20e1bf0e
ytk6565 <ytk6565@gmail.com>
2022-12-25 00:28:12 +0900

    vue2 のアプリケーションを追加

---
+++ apps/vue2/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <VCounter />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue-demi";
+import { VCounter } from "@packages/ui";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    VCounter,
+  },
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
