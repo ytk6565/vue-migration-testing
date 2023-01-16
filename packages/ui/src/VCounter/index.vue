a369aa6fb1d80f9c392850c3131b2fadd463fd00
Yutaka Kaneko <ytk6565@gmail.com>
2023-01-16 16:55:07 +0900

    CI が落ちるため VButton を Button に変更

--- packages/ui/src/VCounter/index.vue
+++ packages/ui/src/VCounter/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <p data-cy="counter">Times clicked: {{ count }}</p>
-    <v-button @click="increment" data-cy="increment">increment</v-button>
-    <v-button @click="decrement" data-cy="decrement">decrement</v-button>
+    <button @click="increment" data-cy="increment">increment</v-button>
+    <button @click="decrement" data-cy="decrement">decrement</v-button>
   </div>
 </template>
 
@@ -10,12 +10,8 @@
 import { defineComponent } from "vue-demi";
 
 import { useCounter } from "@/VCounter/compositions/useCounter";
-import VButton from "@/VButton/index.vue";
 
 export default defineComponent({
-  components: {
-    VButton,
-  },
   setup() {
     const { count, increment, decrement } = useCounter();
 
